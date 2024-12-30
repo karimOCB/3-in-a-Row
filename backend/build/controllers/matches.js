84609763b542f79c086a1701cc293f111c6960b4
Chaya <chaya.karim.omar@gmail.com>
2024-12-30 20:06:02 -0300

    0.39

--- backend/build/controllers/matches.js
+++ backend/build/controllers/matches.js
@@ -27,13 +27,13 @@ const setCookies = (res, accessToken, refreshToken) => {
     res.cookie('accessToken', accessToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 15 * 60 * 1000
     });
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "development",
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 7 * 24 * 60 * 60 * 1000
     });
 };
